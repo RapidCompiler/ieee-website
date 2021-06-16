4f79a71dfc1ade74f417c206d48e18df796cf6c9
RapidCompiler <59367217+RapidCompiler@users.noreply.github.com>
2021-06-16 12:25:04 +0530

    Added + character in the home page counter

--- js/main.js
+++ js/main.js
@@ -192,4 +192,18 @@
         });
     });
 
+    $('.counter_num_plus').each(function () {
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 4000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now) + '+');
+            }
+        });
+    });
+
+
+
 })(jQuery);
